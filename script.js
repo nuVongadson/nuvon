54585e31c523d3cebe11c519921fd7cf6ca9701b
nuVongadson <nuvongadson@gmail.com>
2024-09-01 01:20:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+// Additional interactive JavaScript functions can be added here
